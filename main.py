10cecd8b2ba1018a2c92d654f1f24163187950ae
Admiin <john20040409@gmail.com>
2021-10-15 18:55:23 +0300

    Добавил проверку конца игры (до тех пор, пока есть свободное место); теперь ставить в самые крайние узлы точки нельзя (что, наверное, правильно)

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ SECOND = 1
 
 class Game(object):
 	"""Класс, отвечающий за основной игровой процесс"""
-
-	def __init__(self, title: str, width: int, height: int, fps: int, size=(20, 20), scale=30):
-		self.game_over = False  # Флаг конца игры
+	def __init__(self, title: str, width: int, height: int, fps: int, size=(10, 10)):
 		# Инициализация звука, графики, часов, etc:
 		pygame.mixer.pre_init(44100, 16, 2, 4096)
 		pygame.init()
@@ -18,13 +16,13 @@ class Game(object):
 		pygame.display.set_caption(title)
 		self.clock = pygame.time.Clock()
 		self.fps = fps  # Частота смены кадров
-		self.scale = scale  # Размер клетки
+		self.scale = width // size[0]  # Размер клетки
 		self.size = size  # Количество клеток
 		self.cell_color = pygame.Color((5, 4, 170))  # Цвет клеток (королевский синий)
 		self.screen_color = pygame.Color("white")  # Цвет фона
 		self.dot_color_1 = pygame.Color("red")  # Цвет точки одного игрока
 		self.dot_color_2 = pygame.Color("blue")  # Цвет точки другого игрока
-		self.field = [[None for _ in range(20)] for _ in range(20)]  # Клеточное поле, собственно
+		self.field = [[None for _ in range(size[0])] for _ in range(size[1])]  # Клеточное поле, собственно
 		self.move = FIRST
 
 	def draw(self):
@@ -36,14 +34,17 @@ class Game(object):
 			pygame.draw.line(
 				self.surface, self.cell_color, [0, i * self.scale], [self.size[0] * self.scale, i * self.scale])
 
-		# Можно этот цикл убрать в конец, мне больше понравились линейки поверх точек
 		for i in range(len(self.field)):
 			for j in range(len(self.field[0])):
 				if self.field[i][j]:
 					self.field[i][j].draw(self.surface)
 
 	def handle_mouse_click(self, x: int, y: int):
+		"""Обработка нажатия на кнопку мыши: преобразуются координаты, заполняется игровое поле"""
 		x_true, y_true = x // self.scale, y // self.scale
+		if x_true == 0 or x_true == 19 or y_true == 0 or y_true == 19:
+			# Дополнительная проверка: не будем ставить в самый край листочка
+			return
 		if self.move == FIRST:
 			if not self.field[x_true][y_true]:
 				# Если клетка уже не занята!
@@ -54,6 +55,7 @@ class Game(object):
 		self.change_move()
 
 	def change_move(self):
+		"""Смена хода текущего игрока"""
 		if self.move == FIRST:
 			self.move = SECOND
 		else:
@@ -72,23 +74,45 @@ class Game(object):
 
 	def run(self):
 		"""Игровой процесс - обрабатываем события, обновляем, отрисовываем..."""
-		while not self.game_over:
+		while self.is_accessible_field():
+			self.check_points()
 			self.surface.fill(self.screen_color)
 			self.handle_events()
 			self.draw()
 			pygame.display.update()
 			self.clock.tick(self.fps)
 
+	def is_accessible_field(self) -> bool:
+		"""Проверяем, есть ли свободное место для клеток на поле"""
+		for array in self.field[1:self.size[0] - 1]:
+			if not all(array[1:self.size[1] - 1]):
+				# Мы не учитываем самые крайние узлы - они не заполняются!
+				return True
+		return False
+
+	def check_points(self):
+		"""Проверяем, не окружены ли какие-нибудь точки"""
+		for i in range(len(self.field)):
+			for j in range(len(self.field[0])):
+				# Algorithm
+				pass
+
+	def get_player_by_color(self, color_: pygame.Color):
+		"""Возвращает номер игрока по цвету точки"""
+		if color_ == self.dot_color_1:
+			return FIRST
+		return SECOND
+
 
 class Dot(object):
 	"""Класс той самой точки"""
-
 	def __init__(self, x: int, y: int, r: int, color: pygame.Color, scale: int):
 		self.x, self.y = x, y
 		self.radius = r
 		self.diameter = r * 2
 		self.color = color
 		self.scale = scale
+		self.is_active = True  # Не в плену ли точка (в противном случае флаг равен False)
 
 	def draw(self, surface):
 		"""Отрисовка"""
